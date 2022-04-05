14cdceb42bf24286b9e0bbb961bc699cb98e4de5
Akanthos33 <maxime.rebierre@gmail.com>
2022-04-05 10:43:50 +0200

    colonecapital

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_31_105615) do
+ActiveRecord::Schema.define(version: 2022_04_05_083850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
