37948a44083688102e05e884f3fbf49f816e3825
Akanthos33 <maxime.rebierre@gmail.com>
2022-04-05 09:49:06 +0200

    UE

--- db/migrate/20220331105615_create_countries.rb
+++ db/migrate/20220331105615_create_countries.rb
@@ -2,7 +2,9 @@ class CreateCountries < ActiveRecord::Migration[6.1]
   def change
     create_table :countries do |t|
       t.string :name
+      t.string :capital
       t.text :flag
+
 
       t.timestamps
     end
